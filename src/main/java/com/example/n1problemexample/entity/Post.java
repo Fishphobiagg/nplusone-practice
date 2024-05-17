5c7d0a607801bb02b68457dd0897cc03cf6e0d7c
fishphobiagg <dlauddn6211@icloud.com>
2024-05-17 19:35:10 +0900

    feat:엔티티 객체 코드 작성

---
+++ src/main/java/com/example/n1problemexample/entity/Post.java
@@ -0,0 +1,52 @@
+package com.example.n1problemexample.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "post")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+	private String content;
+
+	@OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+	private List<Comment> comments = new ArrayList<>();
+
+
+	@Builder
+	public Post(String title, String content){
+		this.title = title;
+		this.content = content;
+	}
+
+	public void addComment(String content){
+		this.comments.add(buildComment(content));
+	}
+
+	private Comment buildComment(String content){
+		return Comment.builder()
+			.post(this)
+			.content(content)
+			.build();
+	}
+
+}
